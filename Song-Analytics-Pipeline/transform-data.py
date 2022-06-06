e88ae6b39a2d7264f24514a5267c084929a1e17c
Jenil Patel <jenil2452000@gmail.com>
2022-06-06 14:55:00 -0400

    read the logs data from s3

--- Song-Analytics-Pipeline/transform-data.py
+++ Song-Analytics-Pipeline/transform-data.py
@@ -78,28 +78,7 @@ def read_s3_data(spark, file_location, file_type, schema):
 
 # COMMAND ----------
 
-songs_file_location = "/mnt/song-data/songs_data.json"
-songs_file_type = "json"
-
-song_schema = StructType([
-        StructField("num_songs", IntegerType()),
-        StructField("artist_id", StringType()),
-        StructField("artist_latitude", FloatType()),
-        StructField("artist_longitude", FloatType()),
-        StructField("artist_location", StringType()),
-        StructField("artist_name", StringType()),
-        StructField("song_id", StringType()),
-        StructField("title", StringType()),
-        StructField("duration", FloatType()),
-        StructField("year", IntegerType())
-    ])
-
-songs_df = read_s3_data(spark, songs_file_location, songs_file_type, song_schema)
-songs_df.head(5)
-
-# COMMAND ----------
-
-def process_songs_data(spark, df):
+def process_songs_dataframe(spark, df):
     '''
     Process songs dataframe to organize the tables and clean data
     
@@ -126,4 +105,92 @@ def process_songs_data(spark, df):
 
 # COMMAND ----------
 
-original_songs_df, songs_df, artists_df = process_songs_data(spark, songs_df)
+def process_logs_dataframe(spark, df):
+    '''
+    Process logs dataframe to organize the tables and clean data
+    
+    Parameters:
+    arg1 (Spark): Spark object
+    arg2 (DataFrame): logs dataframe
+    
+    Returns (DataFrames): Returning organized dataframes
+    '''
+
+    paid_users = df.select(['userId', 'level']).filter( df['level'] == 'paid')
+    paid_users = paid_users.groupby(['userId']).count()
+    free_users = df.select(['userId', 'level']).filter( df['level'] == 'free')
+    free_users = free_users.groupby(['userId']).count()
+    paid_users.createOrReplaceTempView('paid_users')
+    free_users.createOrReplaceTempView('free_users')
+    user_listen = spark.sql("""
+        select a.userId, a.count puCount, b.count fuCount
+        from paid_users a join free_users b
+        on a.userId = b.userId
+        where a.userId != ''
+    """)        
+    
+    print(user_listen.show())
+    
+    
+
+# COMMAND ----------
+
+songs_file_location = "/mnt/song-data/songs_data.json"
+songs_file_type = "json"
+
+song_schema = StructType([
+        StructField("num_songs", IntegerType()),
+        StructField("artist_id", StringType()),
+        StructField("artist_latitude", FloatType()),
+        StructField("artist_longitude", FloatType()),
+        StructField("artist_location", StringType()),
+        StructField("artist_name", StringType()),
+        StructField("song_id", StringType()),
+        StructField("title", StringType()),
+        StructField("duration", FloatType()),
+        StructField("year", IntegerType())
+    ])
+
+songs_df = read_s3_data(spark, songs_file_location, songs_file_type, song_schema)
+songs_df.head(5)
+
+# COMMAND ----------
+
+logs_file_location = "/mnt/song-data/logs.json"
+logs_file_type = "json"
+
+log_schema = StructType([
+        StructField("artist", StringType()),
+        StructField("auth", StringType()),
+        StructField("firstName", StringType()),
+        StructField("gender", StringType()),
+        StructField("itemInSession", LongType()),
+        StructField("lastName", StringType()),
+        StructField("length", DoubleType()),
+        StructField("level", StringType()),
+        StructField("location", StringType()),
+        StructField("method", StringType()),
+        StructField("page", StringType()),
+        StructField("registration", DoubleType()),
+        StructField("sessionId", LongType()),
+        StructField("song", StringType()),
+        StructField("status", StringType()),
+        StructField("ts", StringType()),
+        StructField("userAgent", StringType()),
+        StructField("userId", StringType())
+    ])
+
+logs_df = read_s3_data(spark, logs_file_location, logs_file_type, log_schema)
+logs_df.head(5)
+
+# COMMAND ----------
+
+original_songs_df, songs_df, artists_df = process_songs_dataframe(spark, songs_df)
+
+# COMMAND ----------
+
+process_logs_dataframe(spark, logs_df)
+
+# COMMAND ----------
+
+
