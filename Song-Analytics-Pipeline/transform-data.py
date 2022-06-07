29ee4480635b079c413c96426e9a9dab8125874e
Jenil Patel <jenil2452000@gmail.com>
2022-06-07 12:46:18 -0400

    finished processing log data by extracting useful information into different tables

--- Song-Analytics-Pipeline/transform-data.py
+++ Song-Analytics-Pipeline/transform-data.py
@@ -1,6 +1,9 @@
 # Databricks notebook source
 from pyspark.sql.types import StructType, StructField
 from pyspark.sql.types import StringType, IntegerType, FloatType, TimestampType, LongType, DoubleType
+import datetime #Required for ts conversion
+from pyspark.sql.functions import udf
+from pyspark.sql import functions as F
 from pyspark.sql.functions import year, month, dayofmonth, hour, minute, second
 
 # COMMAND ----------
@@ -30,11 +33,12 @@ def create_spark_session():
 file_type = "csv"
 first_row_is_header = "true"
 delimiter = ","
+file_location = "dbfs:/FileStore/shared_uploads/jenil2452000@gmail.com/song_project_credentials.csv"
 
 AWS_credentials = spark.read.format(file_type)\
 .option("header", first_row_is_header)\
 .option("sep", delimiter)\
-.load("dbfs:/FileStore/shared_uploads/jenil2452000@gmail.com/song_project_credentials.csv")
+.load(file_location)
 
 #Set up the credential variables
 AWS_ACCESS_KEY = AWS_credentials.collect()[0]['Access key ID']
@@ -93,7 +97,7 @@ def process_songs_dataframe(spark, df):
     df = df.fillna({'artist_latitude':0})
     df = df.fillna({'artist_longitude':0})
     
-    songs_df = df.select(['song_id', 'title', 'artist_id', 'year', 'duration']).dropDuplicates()
+    songs_info_df = df.select(['song_id', 'title', 'artist_id', 'year', 'duration']).dropDuplicates()
     
     artists_df = df.select(['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude'])\
                     .withColumnRenamed('artist_name','name') \
@@ -101,7 +105,7 @@ def process_songs_dataframe(spark, df):
                     .withColumnRenamed('artist_latitude','latitude') \
                     .withColumnRenamed('artist_longitude','longitude').dropDuplicates()
     
-    return df, songs_df, artists_df
+    return df, songs_info_df, artists_df
 
 # COMMAND ----------
 
@@ -115,23 +119,64 @@ def process_logs_dataframe(spark, df):
     
     Returns (DataFrames): Returning organized dataframes
     '''
-
+    
+    # only take users who are listening to music continuously
+    df = df.filter(df['page']=='NextSong')
+    
+    # Convert ts from long to datetime
+    # Timestamp is in  milliseconds so we need to divide it by 1000 otherwise year out of range error will be displayed
+    convert_time = udf(lambda x: datetime.datetime.fromtimestamp(float(x) / 1000.0), TimestampType())
+    df = df.withColumn("ts_converted", convert_time(df.ts))
+    
+    # Convert registration from double to long
+    df = df.withColumn("registration_converted", df.registration.cast(LongType()))
+    
+    # creating users table with useful information like full name, gender, level
+    users_table = df.select(['userId', 'firstName', 'lastName', 'gender', 'level'])\
+            .withColumnRenamed('userId', 'user_id')\
+            .withColumnRenamed('firstName', 'first_name')\
+            .withColumnRenamed('lastName', 'last_name').dropDuplicates()
+    
+    users_table.createOrReplaceTempView('users')    
+    
+    
+    # create table that has information on users who have used paid and free version to listen songs
     paid_users = df.select(['userId', 'level']).filter( df['level'] == 'paid')
     paid_users = paid_users.groupby(['userId']).count()
     free_users = df.select(['userId', 'level']).filter( df['level'] == 'free')
     free_users = free_users.groupby(['userId']).count()
+    
     paid_users.createOrReplaceTempView('paid_users')
     free_users.createOrReplaceTempView('free_users')
-    user_listen = spark.sql("""
-        select a.userId, a.count puCount, b.count fuCount
+    
+    user_level_listen = spark.sql("""
+        select a.userId user_id, a.count paid_use_count, b.count free_use_count
         from paid_users a join free_users b
         on a.userId = b.userId
         where a.userId != ''
-    """)        
+    """)      
     
-    print(user_listen.show())
+    user_level_listen.createOrReplaceTempView('user_level_listen')
     
+    user_level_listen = spark.sql("""
+        select distinct u.first_name || ' ' || u.last_name as full_name, ul.paid_use_count, ul.free_use_count
+        from users u join user_listen ul
+        on u.user_id = ul.user_id
+    """)
     
+    # create a table that has all the time stamp information for deeper analytics
+    timestamp_table = df.select(['ts_converted']).withColumnRenamed('ts_converted','start_time') 
+
+    timestamp_table = timestamp_table.withColumn('day', F.dayofmonth('start_time')) \
+                          .withColumn('month', F.month('start_time')) \
+                          .withColumn('year', F.year('start_time')) \
+                          .withColumn('hour', F.hour('start_time')) \
+                          .withColumn('minute', F.minute('start_time')) \
+                          .withColumn('second', F.second('start_time')) \
+                          .withColumn('week', F.weekofyear('start_time')) \
+                          .withColumn('weekday', F.dayofweek('start_time')).dropDuplicates()   
+    
+    return df, users_table, user_level_listen, timestamp_table   
 
 # COMMAND ----------
 
@@ -185,12 +230,8 @@ logs_df.head(5)
 
 # COMMAND ----------
 
-original_songs_df, songs_df, artists_df = process_songs_dataframe(spark, songs_df)
+modified_songs_df, songs_info_df, artists_df = process_songs_dataframe(spark, songs_df)
 
 # COMMAND ----------
 
-process_logs_dataframe(spark, logs_df)
-
-# COMMAND ----------
-
-
+modified_logs_df, users_table, user_level_listen, timestamp_table = process_logs_dataframe(spark, logs_df)
